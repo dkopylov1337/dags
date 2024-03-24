9f46ab50c421f1996dd0801280a5af0e7a3c6e4f
dkopylov1337 <145416404+dkopylov1337@users.noreply.github.com>
2024-03-24 21:07:30 +0300

    Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -1,35 +1,30 @@
 from airflow import DAG
-from airflow.operators.docker_operator import DockerOperator
-from datetime import datetime, timedelta
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 3, 24),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+from airflow.operators.empty import EmptyOperator
+import pendulum
+from airflow.providers.docker.operators.docker import DockerOperator
 
 
-dag = DAG(
-    'uplift_model_retraining',
-    default_args=default_args,
-    description='Daily retraining of uplift models',
-    schedule_interval=timedelta(days=1),
-)
+with DAG(
+    dag_id="uplift_model_retraining", 
+    default_args={
+        "owner": "me",
+    },
+    schedule_interval="@daily", 
+    start_date=pendulum.today('Europe/Moscow').subtract(days=1),
+    tags=["uplift_model", "streamlit", "docker"],
+    catchup=False,
+) as dag:
+    start = EmptyOperator(task_id="start")
 
+    train_and_evaluate = DockerOperator(
+        task_id="train_and_evaluate_models",
+        image="streamlit_homework:latest", 
+        docker_url='unix://var/run/docker.sock',
+        network_mode="bridge",
+        command="streamlit run main.py",  
+        auto_remove=True,
+    )
 
-t1 = DockerOperator(
-    task_id='train_uplift_model',
-    image='streamlit_homework:latest',
-    api_version='auto',
-    auto_remove=True,
-    command='python main.py',
-    docker_url='unix://var/run/docker.sock',
-    network_mode='bridge',
-    dag=dag
-)
+    end = EmptyOperator(task_id="end")
 
-t1
+    start >> train_and_evaluate >> end
